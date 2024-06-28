1535400057643b3c162b8c7a9ca690d539fa8e2c
YonasTewabe <yonastewabe21@gmail.com>
2024-06-28 06:00:27 -0500

    added icon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,23 +1,28 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Habesha Cuisine</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Habesha Cuisine</title>
 
-        <!-- Styles -->
-        <link href="/css/main.css" rel="stylesheet">
-    </head>
-    <body>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-      @yield('content')
+    <!-- Styles -->
+    <link href="/css/main.css" rel="stylesheet">
+    <link rel="icon" href="/images/image.png">
 
-      <footer>
+</head>
+
+<body>
+
+    @yield('content')
+
+    <footer>
         <p>Copyright 2024 Habesha Cuisine</p>
-      </footer>
-    </body>
+    </footer>
+</body>
+
 </html>
